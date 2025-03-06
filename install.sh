6cc6c52587feb7deb60042ba22f260854c3341ac
Kaiwinta <titouan.pradoura@epitech.eu>
2025-03-06 18:30:31 +0100

    [fix]: Invalid scirpt path in alias

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+SCRIPT_FULL_PATH="$(realpath "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_FULL_PATH")"
+SCRIPT_PATH=usefull_scripts
 
+echo $PATH
 add_alias() {
     file="$1"
     alias_name="$2"
@@ -15,14 +19,19 @@ add_alias() {
 }
 
 echo "Setting up pushnorme"
-add_alias ~/.bashrc push "./{path}/pushnorme.sh"
-add_alias ~/.zshrc push "./{path}/pushnorme.sh"
+add_alias ~/.bashrc push "$SCRIPT_DIR/$SCRIPT_PATH/pushnorme.sh"
+add_alias ~/.zshrc push "$SCRIPT_DIR/$SCRIPT_PATH/pushnorme.sh"
 
 echo "Setting up epitech-coding-style-checker"
-git clone git@github.com:Epitech/coding-style-checker.git ~/.epitech-coding-style-checker
+if [[ ! -d ~/.epitech-coding-style-checker ]]; then
+    echo "Cloning epitech-coding-style-checker"
+    git clone git@github.com:Epitech/coding-style-checker.git ~/.epitech-coding-style-checker
+else
+    echo "epitech-coding-style-checker already exists"
+fi
 add_alias ~/.bashrc style "~/.epitech-coding-style-checker/coding-style.sh . ."
-add_alias ~/.zshrc style "~/.epitech-coding-style-checker/coding-style.sh . ."
+add_alias ~/.zshrc style "~/.epitech-coding-style-checker/coding-style.sh . . ; cat coding-style-reports.log"
 
 echo "Setting up init_project"
-add_alias ~/.bashrc init_project "./{path}/init_project.sh"
-add_alias ~/.zshrc init_project "./{path}/init_project.sh"
+add_alias ~/.bashrc init_project "$SCRIPT_DIR/$SCRIPT_PATH/init_project.sh"
+add_alias ~/.zshrc init_project "$SCRIPT_DIR/$SCRIPT_PATH/init_project.sh"
--- usefull_scripts/init_project.sh
+++ usefull_scripts/init_project.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 TYPE=$1
 PROJECT_NAME=$2
 
-TEMPLATE_PATH="templates"
+SCRIPT_FULL_PATH="$(realpath "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_FULL_PATH")"
+
+TEMPLATE_PATH="$SCRIPT_DIR/templates"
 
 if [[ -z "$PROJECT_NAME" ]]; then
     echo "Usage: init_project.sh [type] [project_name]"
@@ -12,8 +17,8 @@ fi
 base_project() {
     FOLDER=$1
 
-    echo "Initializing $FOLDER project $PROJECT_NAME"
-    echo "Creating project structure"
+    echo "Initializing $FOLDER project: $PROJECT_NAME"
+    echo "Creating project structure..."
     mkdir -p include src
 
     if [[ -f "$TEMPLATE_PATH/$FOLDER/.gitignore" ]]; then
@@ -31,17 +36,21 @@ base_project() {
     fi
 
     if [[ -d "$TEMPLATE_PATH/$FOLDER/src" ]]; then
-        cp -r "$TEMPLATE_PATH/$FOLDER/src" .
+        cp -r "$TEMPLATE_PATH/$FOLDER/src/" src/
     else
         echo "src/ not found in $TEMPLATE_PATH/$FOLDER"
     fi
 
     touch include/.gitkeep
-    echo "Project $PROJECT_NAME initialized"
+    echo "Project '$PROJECT_NAME' initialized successfully!"
 }
 
 if [[ "$TYPE" == "-C" ]]; then
     base_project C
 elif [[ "$TYPE" == "-CPP" ]]; then
     base_project CPP
+else
+    echo "Invalid project type: $TYPE"
+    echo "Valid options: -C, -CPP"
+    exit 1
 fi
