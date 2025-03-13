5a1566a114edae2c62fd6267dd5e616706582866
Kaiwinta <titouan.pradoura@epitech.eu>
2025-03-13 12:37:27 +0100

    [feat]: C and Cpp templates with workflows

---
+++ usefull_scripts/templates/C/src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** Scripts
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+    printf("Hello World\n");
+    return (0);
+}
