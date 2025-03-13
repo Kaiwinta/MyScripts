b76676e1d08da805c5681f9d0e2d99a98e023494
Kaiwinta <titouan.pradoura@epitech.eu>
2025-03-13 14:10:25 +0100

    [feat]: Color functions

--- install.sh
+++ install.sh
@@ -4,25 +4,54 @@ SCRIPT_FULL_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_FULL_PATH")"
 SCRIPT_PATH=usefull_scripts
 
-echo $PATH
 add_alias() {
     file="$1"
     alias_name="$2"
     alias_command="$3"
     
     if grep -q "alias $alias_name=" "$file"; then
-        echo "Alias '$alias_name' already exists in $file"
+        echo -e "\033[0;33mAlias '$alias_name' already exists\033[0;0m in $file"
     else
+        echo -n "Adding alias '$alias_name' to $file"
         echo "alias $alias_name=\"$alias_command\"" >> "$file"
-        echo "Added alias '$alias_name' to $file"
+        echo -e "\rAdding alias '$alias_name' to $file\033[35;m done\033[0m\n"
     fi
 }
 
-echo "Setting up pushnorme"
+install_colors() {
+    file="$1"
+
+    if grep -q "Color functions" "$file"; then
+        echo "Colors already installed in $file"
+    else
+        echo 'RED() { echo -ne "\033[0;31m"; }' >> "$file"
+        echo 'GREEN() { echo -ne "\033[0;32m"; }' >> "$file"
+        echo 'YELLOW() { echo -ne "\033[0;33m"; }' >> "$file"
+        echo 'BLUE() { echo -ne "\033[0;34m"; }' >> "$file"
+        echo 'MAGENTA() { echo -ne "\033[0;35m"; }' >> "$file"
+        echo 'CYAN() { echo -ne "\033[0;36m"; }' >> "$file"
+        echo 'WHITE() { echo -ne "\033[0;37m"; }' >> "$file"
+        echo 'BOLD_RED() { echo -ne "\033[1;31m"; }' >> "$file"
+        echo 'BOLD_GREEN() { echo -ne "\033[1;32m"; }' >> "$file"
+        echo 'BOLD_YELLOW() { echo -ne "\033[1;33m"; }' >> "$file"
+        echo 'BOLD_BLUE() { echo -ne "\033[1;34m"; }' >> "$file"
+        echo 'BOLD_MAGENTA() { echo -ne "\033[1;35m"; }' >> "$file"
+        echo 'BOLD_CYAN() { echo -ne "\033[1;36m"; }' >> "$file"
+        echo 'BOLD_WHITE() { echo -ne "\033[1;37m"; }' >> "$file"
+        echo 'RESET() { echo -ne "\033[0m"; }' >> "$file"
+    fi
+}
+
+echo -ne "\033[0;34mInstalling colors...\033[0m"
+install_colors ~/.bashrc
+install_colors ~/.zshrc
+echo -ne "\r\033[0;34mInstalling colors...\033[0m\033[35;m done\033[0m\n"
+
+echo -ne "\033[0;34mSetting up pushnorme\033[0m\n"
 add_alias ~/.bashrc push "$SCRIPT_DIR/$SCRIPT_PATH/pushnorme.sh"
 add_alias ~/.zshrc push "$SCRIPT_DIR/$SCRIPT_PATH/pushnorme.sh"
 
-echo "Setting up epitech-coding-style-checker"
+echo -ne "\033[0;34mSetting up epitech-coding-style-checker\033[0m\n"
 if [[ ! -d ~/.epitech-coding-style-checker ]]; then
     echo "Cloning epitech-coding-style-checker"
     git clone git@github.com:Epitech/coding-style-checker.git ~/.epitech-coding-style-checker
@@ -36,7 +65,11 @@ echo "Setting up init_project"
 add_alias ~/.bashrc init_project "$SCRIPT_DIR/$SCRIPT_PATH/init_project.sh"
 add_alias ~/.zshrc init_project "$SCRIPT_DIR/$SCRIPT_PATH/init_project.sh"
 
+
 echo "Setting up Epitech environment"
-git clone git@github.com:floriangolling/epitech_tester.git ~/.epitech_tester_installer
-chmod 777 ~/.epitech_tester_installer/setup.sh
-sudo ~/.epitech_tester_installer/setup.sh
+if [[ ! -d ~/.epitech_tester_installer ]]; then
+    echo "Cloning epitech_tester"
+    git clone git@github.com:floriangolling/epitech_tester.git ~/.epitech_tester_installer
+    chmod 777 ~/.epitech_tester_installer/setup.sh
+    sudo ~/.epitech_tester_installer/setup.sh
+fi
