8e38de8743b7249cf6b64a70b55fd9f80ef59f7c
Kaiwinta <titouan.pradoura@epitech.eu>
2025-04-07 11:03:48 +0200

    [fix]: haskell template headers

--- usefull_scripts/templates/Hs/app/Main.hs
+++ usefull_scripts/templates/Hs/app/Main.hs
@@ -1,3 +1,10 @@
+{-
+-- EPITECH PROJECT, 2025
+-- MyScripts
+-- File description:
+-- Main
+-}
+
 module Main (main) where
 
 import Lib
--- usefull_scripts/templates/Hs/src/Lib.hs
+++ usefull_scripts/templates/Hs/src/Lib.hs
@@ -1,3 +1,10 @@
+{-
+-- EPITECH PROJECT, 2025
+-- MyScripts
+-- File description:
+-- Lib
+-}
+
 module Lib
     ( someFunc
     ) where
