df99e33e1275dd3649540365a7131f715638be97
Kaiwinta <titouan.pradoura@epitech.eu>
2025-05-01 10:50:48 +0200

    [feat]: Launching make tests_run before pushing, adding --force to bypass

--- usefull_scripts/pushnorme.sh
+++ usefull_scripts/pushnorme.sh
@@ -2,9 +2,10 @@
 
 TYPE=$1
 COMMENT=$2
+FORCE=$3
 
 if [[ -z "$COMMENT" && "$TYPE" != "-h" ]]; then
-    echo "Usage: pushnorme.sh [type] [comment]"
+    echo "Usage: pushnorme.sh [type] [comment] [--force]"
     echo "[comment] is missing"
     exit 1
 fi
@@ -21,24 +22,35 @@ case "$TYPE" in
     "-b") MESSAGE="[build]: $COMMENT" ;;
     "-m") MESSAGE="[merge]: $COMMENT" ;;
     "-h") 
-        echo "Usage: pushnorme.sh [type] [comment]"
+        echo "Usage: pushnorme.sh [type] [comment] [--force]"
         echo "type: -f fix, -d doc, -rm del, -s style, -t test, -b build, -m merge -p perf, -r refactor, -h help"
         echo "type: -h help"
         echo "type: feature being the default (Use pushnorme.sh . [comment])"
         echo "comment: comment of the commit"
+        echo "force: --force to force the commit even if the Makefile fails"
         exit 0
         ;;
     *) MESSAGE="[feat]: $COMMENT" ;;
 esac
 
-if [[ -f Makefile ]]; then
-    make -j16 || { 
+if [[ -f Makefile && (-z "$FORCE" || "$FORCE" != "--force") ]]; then
+    make fclean && make -j16 || { 
         echo "Make failed, attempting full clean rebuild..."
         make fclean && make -j16 && make fclean || {
             echo "Error: Build failed even after full clean"
             exit 1
         }
     }
+    if make tests_run > /dev/null 2>&1; then
+        echo "Tests executed successfully."
+    else
+        if make tests_run 2>&1 | grep -q "No rule to make target 'tests_run'"; then
+            echo "No tests found, continuing..."
+        else
+            echo "Tests failed."
+            exit 1
+        fi
+    fi
 fi
 
 if  git branch | grep -q "* main" ; then
@@ -51,7 +63,10 @@ if  git branch | grep -q "* main" ; then
     fi
 fi
 
-git commit -m "$MESSAGE"
+if git commit -m "$MESSAGE" | grep "nothing to commit" ; then
+    echo "Quitting due to absence of changes"
+    exit 0
+fi
 git push 2> /tmp/pushnorme_error
 
 
