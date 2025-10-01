51d5abcfc79582133367ef253d91e883c6f5da2d
Titouan <titouan.pradoura@epitech.eu>
2025-10-01 14:25:21 +0200

    fix: Username being Jesse by defautl in github

--- setup_scripts/setup_github.sh
+++ setup_scripts/setup_github.sh
@@ -16,15 +16,25 @@ getemail () {
     fi
 }
 
+getusername () {
+    echo "Enter your github username:"
+    read -r USERNAME
+    if [[ -z "$USERNAME" ]]; then
+        echo "Username cannot be empty. Please try again."
+        getusername
+    fi
+}
+
 setup_github () {
     echo "Do you want to setup github ssh?"
     read -p "y/n: " GITHUB_SSH
     if [ "$GITHUB_SSH" == "y" ]; then
         echo "Configurating github"
         getemail
+        getusername
         ssh-keygen -t ed25519 -C "$EMAIL"
         git config --global user.email "$EMAIL"
-        git config --global user.name "Jesse"
+        git config --global user.name "$USERNAME"
         installIfNeeded github-cli
         gh auth login
     fi
