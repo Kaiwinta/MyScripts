ad9a989631bad59f8592492c27d5b1eaac7a3583
Kaiwinta <titouan.pradoura@epitech.eu>
2025-12-17 14:39:07 +0100

    fix: Cloning both epitechs

--- setup_scripts/setup_epitech_repo.sh
+++ setup_scripts/setup_epitech_repo.sh
@@ -1,36 +1,55 @@
 #/bin/bash
 
 ORG_NAME="EpitechPromo2028"
+TEK3_ORG_NAME="EpitechPGE3-2025"
 
 YEARS="tek1"
 MODULE="OTHER"
+
+clone_one_roga() {
+    ORG_NAME="$1"
+    REMOVE_GIT="$2"
+
+    gh repo list "$ORG_NAME" --limit 1000 --json name --jq '.[].name' | while read -r repo; do
+
+        if [[ "$repo" =~ ^[BG]-([A-Z]{3})-([0-9]{3})-STG-([0-9]+)-[0-9]+-([^-]+) ]]; then
+            MODULE="${BASH_REMATCH[1]}"
+            LEVEL="${BASH_REMATCH[2]}"
+            SEMESTER="${BASH_REMATCH[3]}"
+            PROJECT_NAME="${BASH_REMATCH[4]}"
+        else
+            echo "Skipping unrecognized repo: $repo"
+            continue
+        fi
+
+        if (( LEVEL < 300 )); then
+            YEARS="tek1"
+        elif (( LEVEL < 500 )); then
+            YEARS="tek2"
+        else
+            YEARS="tek3"
+        fi
+
+        TARGET_DIR="./Epitech/$YEARS/Semester$SEMESTER/$MODULE/$PROJECT_NAME"
+
+        echo "Cloning $repo → $TARGET_DIR"
+        mkdir -p "$TARGET_DIR"
+        git clone "git@github.com:$ORG_NAME/$repo.git" "$TARGET_DIR"
+        FILE_COUNT=$(ls "$TARGET_DIR" | wc -l)
+        if (( FILE_COUNT == 0 )); then
+            rm -rf "$TARGET_DIR"
+        elif [[ "$REMOVE_GIT" == "true" ]]; then
+            rm -rf "$TARGET_DIR/.git"
+        fi
+    done
+}
 
 setup_epitech_repo() {
     mkdir ~/Epitech
     mkdir ~/Epitech/tek1
     mkdir ~/Epitech/tek2
-    mkdir ~/Epitech/AER
-    mkdir ~/Epitech/Hub
+    mkdir ~/Epitech/tek3
 
-    gh repo list $ORG_NAME --limit 1000 --json name --jq '.[].name' | while read repo; do
-        if [[ "$repo" =~ -[1-2][0-9]{2} ]]; then
-            YEARS="tek1"
-        elif [[ "$repo" =~ -[3-4][0-9]{2} ]]; then
-            YEARS="tek2"
-        fi
-        if [[ "$repo" =~ B-([A-Za-z]{3})- ]]; then
-            MODULE="${BASH_REMATCH[1]}"
-        elif [[ "$repo" =~ G-([A-Za-z]{3})- ]]; then
-            MODULE="${BASH_REMATCH[1]}"
-        else
-            MODULE="Unknown"
-        fi
-        if [[ "$repo" =~ -([A-Za-z0-9]+)-titouan\.pradoura$ ]]; then
-            PROJECT_NAME="${BASH_REMATCH[1]}"
-        else
-            PROJECT_NAME="$repo"
-        fi
-
-        git clone "git@github.com:$ORG_NAME/$repo.git" ~/Epitech/$YEARS/$MODULE/$PROJECT_NAME
-    done
+    clone_one_roga $ORG_NAME "false"
+    clone_one_roga $TEK3_ORG_NAME "false"
 }
